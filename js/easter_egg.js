752f5a613864207cfbd0d2673fce9101bbb5618e
mrfrost91 <metallfrost@gmail.com>
2018-10-29 19:09:11 +0300

    rm .idea

--- js/easter_egg.js
+++ js/easter_egg.js
@@ -1,6 +1,9 @@
+var snd = [document.getElementsByTagName("audio")[0], document.getElementsByTagName("audio")[1]];
+var word = ["toasty", "ещфыен"];
+var num_char = 0;
+
 function black() {
-    var snd = document.getElementsByTagName("audio")[0];
-    snd.play();
+    snd[0].play();
     document.getElementsByTagName("H3")[4].innerText += " Властелин";
     document.getElementById("vlastelin").removeAttribute("onclick");
     document.getElementsByClassName("values")[4].innerHTML = "<div>9999</div><div>9999</div><div>9999</div>";
@@ -8,8 +11,6 @@ function black() {
 }
 
 function toasty() {
-    var word = ["toasty", "ещфыен"];
-    var num_char = 0;
     document.onkeypress = function match_char(e) {
         var key_char = e.key;
         if (key_char === word[0].charAt(num_char) || (key_char === word[1].charAt(num_char))) {
@@ -18,12 +19,10 @@ function toasty() {
             num_char = 0;
         }
         if (num_char === word[0].length) {
-            var snd = document.getElementsByTagName("audio")[1];
-            snd.play();
+            snd[1].play();
             num_char = 0;
         }
     }
-
 }
 
 window.onkeypress = toasty();
--- js/load.js
+++ js/load.js
@@ -1,20 +1,23 @@
+var xhr = new XMLHttpRequest();
+
 function loadJson() {
-    var xhr = new XMLHttpRequest();
     xhr.overrideMimeType("application/json");
     xhr.open('GET', "http://127.0.0.1:8080/items/", true);
-
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            var jsonArray = JSON.parse(xhr.response);
-            var jsonText, jsonLength, i;
-            jsonLength = jsonArray.length;
-            for (i = 0; i < jsonLength; ++i) {
-                jsonText = "<h3>" + jsonArray[i].id + ". " + jsonArray[i].name + "</h3>";
-                document.getElementsByClassName("heroname")[i].innerHTML = jsonText;
-            }
-        }
-    };
+    xhr.onreadystatechange = getJson;
     xhr.send();
 }
+
+function getJson() {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        var jsonArray = JSON.parse(xhr.response);
+        var jsonText, jsonLength, i;
+        jsonLength = jsonArray.length;
+        for (i = 0; i < jsonLength; ++i) {
+            jsonText = "<h3>" + jsonArray[i].id + ". " + jsonArray[i].name + "</h3>";
+            document.getElementsByClassName("heroname")[i].innerHTML = jsonText;
+        }
+    }
+}
+
+
 window.onload = loadJson;
